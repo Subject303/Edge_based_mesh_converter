088ad38faf3d7e477130af747f075ff5f1592c16
josh <josh8thomas@gmail.com>
2026-05-20 13:41:31 +0100

    123

--- src/src_mpi/main_prog_MPI.f90
+++ src/src_mpi/main_prog_MPI.f90
@@ -1,1 +1,10 @@
-123
+
+
+
+program converter_MPI
+    use iso_fortran_env
+    implicit none
+    
+    
+    
+end program converter_MPI
---
+++ src/src_shared/global_data.f90
@@ -0,0 +1,19 @@
+
+
+module raw_data
+    use iso_fortran_env
+    implicit none
+    
+    
+    
+end module raw_data
+
+
+
+module object_relation_data
+    use iso_fortran_env
+    implicit none
+    
+    
+    
+end module object_relation_data
