77455ca1df92743e65755c179345e828cbb610e4
josh <josh8thomas@gmail.com>
2026-05-28 16:47:27 +0100

    123

--- python_scripts/case_preprocessor.py
+++ python_scripts/case_preprocessor.py
@@ -22,7 +22,7 @@ cdp = vtkCompositeDataPipeline()
 # might not be needed
 algo.SetDefaultExecutivePrototype(cdp)
 del cdp
-algo.SetCaseFileName("/case/star.case") 
+algo.SetCaseFileName("../case/star.case") 
 # will want to change to somthing more generic
 algo.Update()
 raw_data = algo.GetOutput()
