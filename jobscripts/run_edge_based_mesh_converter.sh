b7fc29fc24a4f8dceace98afbcb9ff87df671458
josh <josh8thomas@gmail.com>
2026-06-01 13:35:57 +0100

    123

--- jobscripts/run_edge_based_mesh_converter.sh
+++ jobscripts/run_edge_based_mesh_converter.sh
@@ -3,30 +3,24 @@
 
 
 COMPLIST=" ../src/src_shared/* "
-echo $complist
 
-# COMPLIST=$COMPLIST" ../src/src_serial/*"
-# 
-# LIB_LIST="./libs/lib "
-# 
-# INCLUDE_LIST="./libs/include "
-# 
-# #COMP_OPTIONS=" -fdec-format-defaults -ffree-line-length-none -g3 -fcheck=all -fbacktrace -ffpe-trap=zero,overflow  "
-# COMP_OPTIONS=" -fdec-format-defaults -ffree-line-length-none -ffpe-trap=zero,overflow  "
-# 
-# module purge
-# module load METIS/5.1.0-GCCcore-12.2.0 
-# module load GCC
-# 
-# #IMPORT CONFIGS
-# set -a
-# source "./config/Testconfig.env"
-# set +a
-# 
-# mkdir -p  "./modfiles"
-# 
-# mpifort $COMP_OPTIONS -o ./Flowsolver $COMPLIST -I$INCLUDE_LIST -L$LIB_LIST -J"./modfiles" -lfmetis -lmetis
-# 
-# echo ' passed compilation '
-# 
-# mpirun -np $PBS_NP ./Flowsolver
+COMPLIST=$COMPLIST" ../src/src_serial/*"
+
+COMP_OPTIONS=" -fdec-format-defaults -ffree-line-length-none -ffpe-trap=zero,overflow  "
+
+module purge
+module load GCC
+
+gfortran $COMP_OPTIONS -o "../converter" $COMPLIST -J"./modfiles"
+
+echo ' passed compilation '
+
+echo ' running python preprocessor '
+
+python3 "../python_scripts/case_preprocessor.py"
+
+echo ' running main converter program serial '
+
+../converter
+
+echo ' finished. '
--- jobscripts/run_edge_based_mesh_converter_MPI_ENABLED.sh
+++ jobscripts/run_edge_based_mesh_converter_MPI_ENABLED.sh
@@ -1,1 +1,33 @@
-123
+#!/bin/bash
+
+
+
+COMPLIST=" ../src/src_shared/* "
+
+COMPLIST=$COMPLIST" ../src/src_mpi/*"
+
+LIB_LIST="./libs/lib "
+
+INCLUDE_LIST="./libs/include "
+
+#COMP_OPTIONS=" -fdec-format-defaults -ffree-line-length-none -g3 -fcheck=all -fbacktrace -ffpe-trap=zero,overflow  "
+COMP_OPTIONS=" -fdec-format-defaults -ffree-line-length-none -ffpe-trap=zero,overflow  "
+
+module purge
+module load GCC
+
+mkdir -p  "./modfiles"
+
+mpifort $COMP_OPTIONS -o "../converter" $COMPLIST -I$INCLUDE_LIST -L$LIB_LIST -J"./modfiles" -lfmetis -lmetis
+
+echo ' passed compilation '
+
+echo ' running python preprocessor '
+
+python3 "../python_scripts/case_preprocessor.py"
+
+echo ' running main converter program parralel with mpi'
+
+mpirun -np $PBS_NP ../converter
+
+echo ' finished. '
