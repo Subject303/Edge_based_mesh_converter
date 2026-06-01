88b8f6f1c7c6c06531f7a7d58a5a115cb4c040ef
josh <josh8thomas@gmail.com>
2026-06-01 13:48:49 +0100

    123

--- jobscripts/run_edge_based_mesh_converter.sh
+++ jobscripts/run_edge_based_mesh_converter.sh
@@ -12,21 +12,21 @@ COMPLIST=$COMPLIST" ../src/src_serial/*"
 
 COMP_OPTIONS=" -fdec-format-defaults -ffree-line-length-none -ffpe-trap=zero,overflow  "
 
-gfortran $COMP_OPTIONS -o "../converter" $COMPLIST -J"../modfiles" &> $LOG
+gfortran $COMP_OPTIONS -o "../converter" $COMPLIST -J"../modfiles" | $LOG
 
 if [ -f "../converter"  ]; then
-    echo " passed compilation " &>> $LOG
+    echo " passed compilation " | $LOG
 else
-    echo "compilation failed" &>> $LOG
+    echo "compilation failed" | $LOG
     exit
 fi
 
-echo ' running python preprocessor ' &>> $LOG
+echo ' running python preprocessor ' | $LOG
 
-python3 "../python_scripts/case_preprocessor.py" &>> $LOG
+python3 "../python_scripts/case_preprocessor.py" | $LOG
 
-echo ' running main converter program serial ' &>> $LOG
+echo ' running main converter program serial ' | $LOG
 
-../converter &>> $LOG
+../converter | $LOG
 
-echo ' finished. ' &>> $LOG
+echo ' finished. ' | $LOG
