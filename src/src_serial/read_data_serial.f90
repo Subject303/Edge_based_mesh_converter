57fb6f1c410307c127bc1b3673d4385e86507c7d
josh <josh8thomas@gmail.com>
2026-06-10 14:34:20 +0100

    123

--- src/src_serial/read_data_serial.f90
+++ src/src_serial/read_data_serial.f90
@@ -75,11 +75,17 @@ module read_data_serial
         print*, 'reading cell to face relation array '
         read(10) c_f_obj_relation_array(:)
 !         
+        print *, c_f_obj_relation_array(1:100)
+        print *, 'AAAAAAAAAAAAAA1'
+        print *, c_f_obj_relation_array((c_f_sum-99):c_f_sum)
+        print *, 'AAAAAAAAAAAAAA1'
 !         print *, 'AAAAAAAAAAAAAA1'
 !         print *, f_e_index_array
         
         print*, 'reading face to edge relation array '
         read(10) f_e_obj_relation_array(:)
+        
+        print *, f_e_obj_relation_array(1:100)
         
         close(10)
         
