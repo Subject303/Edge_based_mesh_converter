e3479dabcf13dd9aba635ba8a4798ad029672db0
josh <josh8thomas@gmail.com>
2026-06-11 20:24:58 +0100

    123

--- python_scripts/case_preprocessor.py
+++ python_scripts/case_preprocessor.py
@@ -303,8 +303,6 @@ c_p_index = temp2
 del temp
 del temp2; gc.collect()
 
-print(f_p_obj_relation_array)
-
 temp = []
 temp2 = []
 j=0
@@ -361,15 +359,11 @@ for i in range(len(f_e_obj_relation_array)):
         obj[j] = edge_to_rep[obj[j]] 
     f_e_obj_relation_array[i] = sorted(obj)
     
-print(c_f_obj_relation_array)
-    
 for i in range(len(c_f_obj_relation_array)):
     obj = c_f_obj_relation_array[i]
     for j in range(len(obj)):
         obj[j] = face_to_rep[obj[j]] 
     c_f_obj_relation_array[i] = sorted(obj)
-
-print(c_f_obj_relation_array)
 
 print('removing duplicate mappings',time.time()-start); sys.stdout.flush()
 
