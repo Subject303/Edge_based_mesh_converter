9032a10de65ac513ff4ea9f92dca56b3e157b9cc
josh <josh8thomas@gmail.com>
2026-06-16 13:32:04 +0100

    123

--- python_scripts/case_preprocessor.py
+++ python_scripts/case_preprocessor.py
@@ -547,7 +547,7 @@ for obj in f_e_obj_relation_array:
 for f in range(nface):
     obj = f_e_obj_relation_array[f]
     print(' face: ', f , ' edges: ' , obj, ' points: ', f_p_obj_relation_array[f])
-    for e in range(len(obj)):
+    for e in obj:
         print(' edge: ',e, ' points: ', e_p_obj_relation_array[e])
   
   
