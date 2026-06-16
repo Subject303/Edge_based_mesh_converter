cba6e7a933b51b024b127fe14052ffbc2ebf302f
josh <josh8thomas@gmail.com>
2026-06-16 13:29:54 +0100

    123

--- python_scripts/case_preprocessor.py
+++ python_scripts/case_preprocessor.py
@@ -546,9 +546,9 @@ for obj in f_e_obj_relation_array:
   
 for f in range(nface):
     obj = f_e_obj_relation_array[f]
-    print(' face: ', f , ' edges: ' , obj, ' points: ', f_p_obj_relation_array[f])
+    print(' face: ', f , ' edges: ' , obj, ' points: ', f_p_obj_relation_array[f]+1)
     for e in range(len(obj)):
-        print(e_p_obj_relation_array[e])
+        print(e_p_obj_relation_array[e]+1)
   
   
 print('finished writing to file',time.time()-start); sys.stdout.flush()
