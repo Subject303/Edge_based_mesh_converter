28f988e2d278f60ffc8b03880f7e94fe0048adcc
wsjt10 <wsjt10@WIN-18450>
2026-06-24 15:56:19 +0100

    123

--- src/src_shared/boundary_preprocessing_module.f90
+++ src/src_shared/boundary_preprocessing_module.f90
@@ -246,8 +246,8 @@ module boundary_routine_module
             
             do i=1,3
                 
-                do p=pf_start,pf_end
-                    if (f_bound_array(p)) p_normal_vectors(bp,i) = p_normal_vectors(bp,i) + f_normal_vectors(reversed_f_bound_indexing_array(p_f_obj_relation_array(p)),i)
+                do f=pf_start,pf_end
+                    if (f_bound_array(p_f_obj_relation_array(f))) p_normal_vectors(bp,i) = p_normal_vectors(bp,i) + f_normal_vectors(reversed_f_bound_indexing_array(p_f_obj_relation_array(f)),i)
                 enddo
                 
             enddo
