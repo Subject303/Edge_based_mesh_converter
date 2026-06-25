306dd6df65b8c1b5ede80adabf8a95a77663e3d4
wsjt10 <wsjt10@WIN-18450>
2026-06-25 17:18:20 +0100

    123

--- src/src_shared/preprocessor_routine_module.f90
+++ src/src_shared/preprocessor_routine_module.f90
@@ -139,11 +139,11 @@ module preprocessor_routine_module
             cf_start_index = 1 + c_f_index_array(c-1)
             cf_end_index   = c_f_index_array(c)
             
-            !print*, 'CELL ' ,c
+            print*, 'CELL ' ,c
             
             n_con_edges = 0
             do cf_index = cf_start_index, cf_end_index
-                !print*, cf_index
+                print*, cf_index, cf_start_index, cf_end_index
                 f = c_f_obj_relation_array(cf_index)
                 
                 n_con_edges =  n_con_edges + f_e_index_array(f) - f_e_index_array(f-1) ! all index arrays start at 0
