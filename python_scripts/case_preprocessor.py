d844b655b86bbbf3118e3e4072517f37edda52d6
wsjt10 <wsjt10@WIN-18450>
2026-06-28 19:23:01 +0100

    123

--- python_scripts/case_preprocessor.py
+++ python_scripts/case_preprocessor.py
@@ -315,7 +315,8 @@ for i in range(len(c_p_index)-1):
     if c_p_index[i] == c_p_index[i+1]:    
         obj1 = c_p_obj_relation_array[i]
         obj2 = c_p_obj_relation_array[i+1]
-        for p in range(c_p_index[i]-1, 0, -1):
+
+        for p in range(c_p_index[i]):
             print(p, obj1[p], obj2[p])
             if obj1[p]!=obj2[p]:
                 temp.append(obj1)
@@ -323,6 +324,15 @@ for i in range(len(c_p_index)-1):
                 j=j+1
                 cell_to_del[i] = 1
                 break
+
+        #for p in range(c_p_index[i]-1, 0, -1):
+        #    print(p, obj1[p], obj2[p])
+        #    if obj1[p]!=obj2[p]:
+        #        temp.append(obj1)
+        #        temp2.append(c_p_index[i])
+        #        j=j+1
+        #        cell_to_del[i] = 1
+         #       break
         print(i,obj1,obj2, cell_to_del[i])
             
 temp.append(c_p_obj_relation_array[i+1])
