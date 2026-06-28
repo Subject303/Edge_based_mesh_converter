48a10ae7d8d7a79f2b4b8e84cb332f17340c4b4e
wsjt10 <wsjt10@WIN-18450>
2026-06-28 22:00:22 +0100

    123

--- python_scripts/case_preprocessor.py
+++ python_scripts/case_preprocessor.py
@@ -398,6 +398,7 @@ temp1=[0]*nface_to_del
 j=0
 for i in face_to_del:
     if 1 == i:
+        print(j, nface_to_del)
         temp[j] = f_e_obj_relation_array[j]
         temp1[j] = f_e_index[j]
     j=j+1
@@ -421,6 +422,7 @@ temp1=[0]*nele_to_del
 j=0
 for i in cell_to_del:
     if 1 == i:
+        print(j, nele_to_del)
         temp[j] = c_f_obj_relation_array[j]
         temp1[j] = c_f_index[j]
     j=j+1
