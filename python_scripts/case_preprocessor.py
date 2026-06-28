578ef085d31b4efbf3b478210ee356c1bf1399e1
wsjt10 <wsjt10@WIN-18450>
2026-06-28 18:17:31 +0100

    123

--- python_scripts/case_preprocessor.py
+++ python_scripts/case_preprocessor.py
@@ -198,8 +198,8 @@ for x, y in coupled:
 del coupled; gc.collect()
 
 for i in range(len(f_sort)-1):
-    print(i, f_sort[i+1], f_sort[i], f_sort[i-1])
-    print( f_p_obj_relation_array[f_sort[i+1]], f_p_obj_relation_array[f_sort[i]], f_p_obj_relation_array[f_sort[i-1]] )
+    print(i, f_sort[i], )
+    print( f_p_obj_relation_array[f_sort[i]] )
 
 for i in range(len(c_f_obj_relation_array)):
     obj = c_f_obj_relation_array[i]
