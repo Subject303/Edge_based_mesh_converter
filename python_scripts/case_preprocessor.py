71e83ecfe6795a0c3fdc450704e732e93cb57cc1
wsjt10 <wsjt10@WIN-18450>
2026-06-28 18:53:11 +0100

    123

--- python_scripts/case_preprocessor.py
+++ python_scripts/case_preprocessor.py
@@ -405,11 +405,21 @@ for i in range(len(f_e_obj_relation_array)):
         obj[j] = edge_to_rep[obj[j]] 
     f_e_obj_relation_array[i] = sorted(obj)
     
+i=0
+for obj in c_f_obj_relation_array:
+    print(i, obj)
+    i=i+1
+
 for i in range(len(c_f_obj_relation_array)):
     obj = c_f_obj_relation_array[i]
     for j in range(len(obj)):
         obj[j] = face_to_rep[obj[j]] 
     c_f_obj_relation_array[i] = sorted(obj)
+
+i=0
+for obj in c_f_obj_relation_array:
+    print(i, obj)
+    i=i+1
 
 temp = []
 temp2 = []
@@ -430,6 +440,7 @@ c_f_obj_relation_array = temp
 c_f_index = temp2
 del temp
 del temp2; gc.collect()
+
 i=0
 for obj in c_f_obj_relation_array:
     print(i, obj)
