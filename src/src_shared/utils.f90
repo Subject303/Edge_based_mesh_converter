bf67055ec8b292b6c3178f29953e2d75247f4a46
wsjt10 <wsjt10@WIN-18450>
2026-06-29 22:46:47 +0100

    123

--- src/src_shared/utils.f90
+++ src/src_shared/utils.f90
@@ -105,7 +105,7 @@ module utils
         dot = v1(1)*v2(1) + v1(2)*v2(2) + v1(3)*v2(3)
         det = v1(1)*v2(2)*v3(3) + v2(1)*v3(2)*v1(3) + v3(1)*v1(2)*v2(3) - v1(3)*v2(2)*v3(1) - v2(3)*v3(2)*v1(1) - v3(3)*v1(2)*v2(1)
         
-        det = det + dot
+        dot = dot + det
         
         planar_alignment_double = ATAN2D(det,dot)
         
