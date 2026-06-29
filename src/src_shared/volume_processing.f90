14738ec6ce452cc69fc481d7b9614572ae5598a9
wsjt10 <wsjt10@WIN-18450>
2026-06-29 15:36:10 +0100

    123

--- src/src_shared/volume_processing.f90
+++ src/src_shared/volume_processing.f90
@@ -937,11 +937,6 @@ module volume_processing
             vol1 = vol1 + abs((c1415(1)*v13(1) ) - (c1415(2)*v13(2) ) + (c1415(3)*v13(3) ) )/6
             vol2 = vol2 + abs((c1415(1)*v32(1) ) - (c1415(2)*v32(2) ) + (c1415(3)*v32(3) ) )/6
             
-            print*, 'aaaaa'
-            print*, c1415
-            print*, c4252
-            print*, 'aaaaa'
-            
 		endif
 
         do i=1,3
@@ -952,6 +947,12 @@ module volume_processing
         sn(1) = sn(1) + ((v34(2) * v35(3) - v34(3) * v35(2))/2) ! projection in the xx axis
         sn(2) = sn(2) + ((v34(3) * v35(1) - v34(1) * v35(3))/2) ! projection in the yy axis
         sn(3) = sn(3) + ((v34(1) * v35(2) - v34(2) * v35(1))/2) ! projection in the zz axis
+        
+
+        print*, 'aaaaa'
+        print*, alignment(v34,v35)
+        print*, 'aaaaa'
+        
         
     !                               i4--------------i5
     !                                \  >>>>>>>   /
