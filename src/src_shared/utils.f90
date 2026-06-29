7bfa75f8c0a216532f6723a50ddcf3df6952a557
wsjt10 <wsjt10@WIN-18450>
2026-06-29 22:51:41 +0100

    123

--- src/src_shared/utils.f90
+++ src/src_shared/utils.f90
@@ -85,7 +85,7 @@ module utils
         dot = v1(1)*v2(1) + v1(2)*v2(2) + v1(3)*v2(3)
         det = v1(1)*v2(2)*v3(3) + v2(1)*v3(2)*v1(3) + v3(1)*v1(2)*v2(3) - v1(3)*v2(2)*v3(1) - v2(3)*v3(2)*v1(1) - v3(3)*v1(2)*v2(1)
                 
-        planar_alignment_single = 180 + ATAN2D(det,dot)
+        planar_alignment_single = ATAN2D(det,dot)
         
     end function planar_alignment_single
     
@@ -105,7 +105,7 @@ module utils
         dot = v1(1)*v2(1) + v1(2)*v2(2) + v1(3)*v2(3)
         det = v1(1)*v2(2)*v3(3) + v2(1)*v3(2)*v1(3) + v3(1)*v1(2)*v2(3) - v1(3)*v2(2)*v3(1) - v2(3)*v3(2)*v1(1) - v3(3)*v1(2)*v2(1)
         
-        planar_alignment_double = 180 + ATAN2D(det,dot)
+        planar_alignment_double = ATAN2D(det,dot)
         
     end function planar_alignment_double
     
