8e3c08ab0fade43ea32ac9d7c56d9c5011eb60be
wsjt10 <wsjt10@WIN-18450>
2026-06-29 22:32:24 +0100

    123

--- src/src_shared/volume_processing.f90
+++ src/src_shared/volume_processing.f90
@@ -889,11 +889,12 @@ module volume_processing
                 v1 = (obj_centroid - baryobj_1_centroid)!centroid_array(1,:) ! vector from 3 to 4
                 v2 = (obj_centroid - baryobj_2_centroid) ! vector from 3 to 5
 				
-                angelee = angelee + planar_alignment(v1,v2,v3)
                 print*, 'aaaaa'
                 print*, baryobj_1_centroid
                 print*, baryobj_2_centroid
                 print*, v3
+                print*, angelee, planar_alignment(v1,v2,v3)
+                angelee = angelee + planar_alignment(v1,v2,v3)
                 print*, angelee, planar_alignment(v1,v2,v3)
                 print*, 'aaaaa'
                 
