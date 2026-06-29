83deacbe93b847eebab72a1b86593e9373963103
wsjt10 <wsjt10@WIN-18450>
2026-06-29 22:58:37 +0100

    123

--- src/src_shared/volume_processing.f90
+++ src/src_shared/volume_processing.f90
@@ -885,8 +885,8 @@ module volume_processing
                 
                 
                 v3 = i1 - i2
-                v1 = (obj_centroid - centroid_array(1,:))
-                !v1 = (obj_centroid - baryobj_1_centroid)
+                !v1 = (obj_centroid - centroid_array(1,:))
+                v1 = (obj_centroid - baryobj_1_centroid)
                 v2 = (obj_centroid - baryobj_2_centroid)
 				
                 print*, 'aaaaa'
