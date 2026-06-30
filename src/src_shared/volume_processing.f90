5b7e4283dac24d64fe1721875a7cfde11e3fa588
wsjt10 <wsjt10@WIN-18450>
2026-06-30 20:26:59 +0100

    123

--- src/src_shared/volume_processing.f90
+++ src/src_shared/volume_processing.f90
@@ -228,10 +228,13 @@ module volume_processing
             print*, 'bound ', e_bound_array(e), e_centroid(e,:)
             print*, 'point'
             print*, e_p_obj_relation_array(i1:i2)
+            print*, p_bound_array(e_p_obj_relation_array(i1:i2))
             print*, 'edge'
             print*, e_f_obj_relation_array(ef_start:ef_end)
+            print*, e_bound_array(e_p_obj_relation_array(i1:i2))
             print*, 'face'
             print*, e_c_obj_relation_array(ec_start:ec_end)
+            print*, c_bound_array(e_p_obj_relation_array(i1:i2))
             
 	    enddo
         
