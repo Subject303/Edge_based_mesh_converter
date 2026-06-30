eea0c3c63e3668faf166d23dd43aa0af4792590c
wsjt10 <wsjt10@WIN-18450>
2026-06-30 17:15:20 +0100

    123

--- src/src_shared/volume_processing.f90
+++ src/src_shared/volume_processing.f90
@@ -899,7 +899,7 @@ module volume_processing
                 print*, baryobj_1_centroid
                 print*, baryobj_2_centroid
                 print*, v3
-                print*, abs(planar_alignment(v1,v2,v3)) - start_angle
+                print*, abs(planar_alignment(v1,v2,v3))! - start_angle
                 !angelee = angelee + abs(planar_alignment(v1,v2,v3))
                 !print*, angelee, planar_alignment(v1,v2,v3)
                 print*, 'aaaaa'
