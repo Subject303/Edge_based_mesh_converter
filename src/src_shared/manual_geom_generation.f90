765a70b52ac7b9ff199e34288667fc45d05ef23c
josh <josh8thomas@gmail.com>
2026-07-13 22:05:16 +0100

    123

--- src/src_shared/manual_geom_generation.f90
+++ src/src_shared/manual_geom_generation.f90
@@ -123,7 +123,7 @@ module manual_geom_generation_module
         
         dOO = 2 * pi / n_rotational
         
-        print*, sind(dOO), cosd(dOO), tand(dOO)
+        print*, pi, dOO, sind(dOO), cosd(dOO), tand(dOO)
             
         npoin = ((n_lengthways*n_highways) + (wake_length*n_highways_wake)) * n_rotational
         
