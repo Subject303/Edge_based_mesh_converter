0e82386b602afa895fa94479b27540b92749b07b
wsjt10 <wsjt10@WIN-18450>
2026-07-16 17:31:04 +0100

    123

--- src/src_shared/manual_geom_generation.f90
+++ src/src_shared/manual_geom_generation.f90
@@ -205,12 +205,12 @@ module manual_geom_generation_module
         j = 1
         do i=1,nele
             
-            k = ceiling(real(j/n_lengthways))
-            l = ceiling(real(j/plane_npoin))
+            k = ceiling(real(j)/n_lengthways)
+            l = ceiling(real(j)/plane_npoin)
             
             print*, '>'
             
-            print*, j, k, l, n_lengthways, plane_npoin,real(j/n_lengthways),real(j/plane_npoin),(j/n_lengthways),(j/plane_npoin)
+            print*, j, k, l, n_lengthways, plane_npoin,real(j)/n_lengthways,real(j)/plane_npoin,(j/n_lengthways),(j/plane_npoin)
             print*, n_lengthways*(k-1), plane_npoin*(l-1), n_lengthways*(k), plane_npoin*(l)
             
             c_p_obj_relation_array(c_p_index_array(i)  ) = n_lengthways*(k-1) + j + 0 + plane_npoin*(l-1)
