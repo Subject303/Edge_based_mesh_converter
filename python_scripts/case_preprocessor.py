b041aac689c39e69c9e652f5fd553dde9b0608ca
josh <josh8thomas@gmail.com>
2026-07-27 17:39:52 +0100

    123

--- python_scripts/case_preprocessor.py
+++ python_scripts/case_preprocessor.py
@@ -25,9 +25,9 @@ cdp = vtkCompositeDataPipeline()
 algo.SetDefaultExecutivePrototype(cdp)
 del cdp
 # algo.SetCaseFileName("./case/tet_sphere.case") #tet sphere
-# algo.SetCaseFileName("./case/2.case") #tiny cube
+algo.SetCaseFileName("./case/2.case") #tiny cube
 #algo.SetCaseFileName("./case/tet_cube.case") #TET cube
-algo.SetCaseFileName("./case/star1.case") # 0.75 mil mesh
+# algo.SetCaseFileName("./case/star1.case") # 0.75 mil mesh
 # algo.SetCaseFileName("./case/star.case") # 24 mil mesh
 # will want to change to somthing more generic
 algo.Update()
