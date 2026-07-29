8732872dd892620beeecb3abbe5298c6b4665a66
josh <josh8thomas@gmail.com>
2026-07-29 15:15:51 +0100

    123

--- python_scripts/case_preprocessor.py
+++ python_scripts/case_preprocessor.py
@@ -547,7 +547,7 @@ print(npoin,nedge,nface,nele)
 print( 'Relation array sums: ')
 print( 'c_p_sum,  f_p_sum,  e_p_sum,  c_f_sum,  f_e_sum')
 print(c_p_sum,f_p_sum,e_p_sum,c_f_sum,f_e_sum)
-print(len(c_f_obj_relation_array),len(f_e_obj_relation_array))
+print(len(c_f_index),len(f_e_index))
 
 print(sum([len(obj) for obj in c_f_obj_relation_array]))
 print(sum([len(obj) for obj in f_e_obj_relation_array]))
