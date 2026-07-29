a81940ec5ab64cb0e287ccf065fd981e69c7cac2
josh <josh8thomas@gmail.com>
2026-07-29 17:01:03 +0100

    123

--- python_scripts/case_preprocessor.py
+++ python_scripts/case_preprocessor.py
@@ -365,9 +365,9 @@ for i in range(nface-1):
         
         for p in range(f_p_index[i]):
             
-            if obj1[p] == 12984:
+            if obj1[p] == 12983:
                 print('obj1, ', k+1)
-            if obj2[p] == 12984:
+            if obj2[p] == 12983:
                 print('obj2, ', k+1)
             
             if obj1[p] != obj2[p]: 
