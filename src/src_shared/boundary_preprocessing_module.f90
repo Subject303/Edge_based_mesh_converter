1eb5d0fc5b97233a7ab765a6303a83ee6a64bd76
josh <josh8thomas@gmail.com>
2026-08-04 20:29:33 +0100

    123

--- src/src_shared/boundary_preprocessing_module.f90
+++ src/src_shared/boundary_preprocessing_module.f90
@@ -434,8 +434,6 @@ module boundary_routine_module
                 
                 print*, 'NEW FLAG ', current_flag, fe_index_old, fe_index, ff_index_old, ff_index
                 
-                ff_index_old = ff_index
-                fe_index_old = fe_index
                 ! skip the next loop because there shouldnt be any connected faces
                 cycle
                 
