742397b7eb4428bccf7ef1035177ce6fccc805d2
josh <josh8thomas@gmail.com>
2026-08-06 19:50:10 +0100

    123

--- src/src_shared/volume_processing.f90
+++ src/src_shared/volume_processing.f90
@@ -295,7 +295,7 @@ module volume_processing
                 sb(be,:) = -in_progress_projection
 			endif
 			
-! 			print*, be, sb(be,:)
+			print*, be, sb(be,:)
             
 !             ef_start = e_f_index_array(e-1)
 !             ec_start = e_c_index_array(e-1)
