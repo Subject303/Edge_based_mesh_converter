a28d08481196234b60eeb46a4933d0e5b75c3388
josh <josh8thomas@gmail.com>
2026-08-06 20:26:58 +0100

    123

--- src/src_serial/data_preprocessing_serial.f90
+++ src/src_serial/data_preprocessing_serial.f90
@@ -129,8 +129,8 @@ module data_processing_serial
         
         call volume_alloc
         
-        call internal_edge_volume_processing
-        call boundary_edge_volume_processing
+!         call internal_edge_volume_processing
+!         call boundary_edge_volume_processing
         call boundary_face_volume_processing
         
         call volume_generation
