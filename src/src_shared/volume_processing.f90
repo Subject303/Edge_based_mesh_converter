27ae1b112a878453ccb848d5f7a7db54aa68ac48
josh <josh8thomas@gmail.com>
2026-08-06 19:15:38 +0100

    123

--- src/src_shared/volume_processing.f90
+++ src/src_shared/volume_processing.f90
@@ -1037,6 +1037,7 @@ module volume_processing
         call centroid_float_assembler(centroid_obj_array, centroid_array, centroid_array_count, obj_type)
         
         do i=1,centroid_array_count
+            print*, centroid_array(i)
             print*,centroid_array(i,:)
         enddo
         
