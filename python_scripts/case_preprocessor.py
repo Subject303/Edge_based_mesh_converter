8b7b01f9c2c49e74d860b13dfd0900f927e24761
josh <josh8thomas@gmail.com>
2026-08-07 19:28:43 +0100

    123

--- python_scripts/case_preprocessor.py
+++ python_scripts/case_preprocessor.py
@@ -28,8 +28,8 @@ del cdp
 # algo.SetCaseFileName("./case/666cube.case") #666 cube
 # algo.SetCaseFileName("./case/2.case") #tiny cube
 # algo.SetCaseFileName("./case/tet_cube.case") #TET cube
-algo.SetCaseFileName("./case/2_rad_sphere_poly.case") #4 diameter poly sphere
-# algo.SetCaseFileName("./case/star1.case") # 0.75 mil mesh
+# algo.SetCaseFileName("./case/2_rad_sphere_poly.case") #4 diameter poly sphere
+algo.SetCaseFileName("./case/star1.case") # 0.75 mil mesh
 # algo.SetCaseFileName("./case/star.case") # 24 mil mesh
 # will want to change to somthing more generic
 algo.Update()
