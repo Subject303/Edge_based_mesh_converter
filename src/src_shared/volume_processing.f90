3418ccba673f9abf10eb46d66cac285fab242347
josh <josh8thomas@gmail.com>
2026-08-07 17:09:30 +0100

    123

--- src/src_shared/volume_processing.f90
+++ src/src_shared/volume_processing.f90
@@ -768,7 +768,16 @@ module volume_processing
 !                 if (centroid_obj_array(fwd_i) .eq. centroid_obj_array(bck_i)) e_state = .true.
                 
             case(featre_point)
-            
+                
+                print*, centroid_obj_array
+                
+                do i=1,centroid_array_count-1,2
+                    print*, centroid_obj_array(i), e_boundary_flags(reversed_e_bound_indexing_array(centroid_obj_array(i)))
+                enddo
+                do i=2,centroid_array_count-2,2
+                    print*, centroid_obj_array(i), f_boundary_flags(reversed_f_bound_indexing_array(centroid_obj_array(i)) )
+                enddo
+                
                 if ((centroid_obj_array(1) .ne. -1) .and. (centroid_obj_array(centroid_array_count-1) .ne. -1)) then
                     if ((e_boundary_flags(centroid_obj_array(centroid_array_count-1)) .eq. 1000) .and. (e_boundary_flags(centroid_obj_array(1)) .eq. 1000)) e_state = .true.
                 endif
