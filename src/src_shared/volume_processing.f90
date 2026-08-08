1ef5d32af5eb46e86eb47cf2c91a5f8662b6a09b
josh <josh8thomas@gmail.com>
2026-08-08 19:40:07 +0100

    123

--- src/src_shared/volume_processing.f90
+++ src/src_shared/volume_processing.f90
@@ -431,7 +431,8 @@ module volume_processing
                     else
                         centroid_array_count = centroid_array_count - 1
                     endif
-                    if (viable_mains(m_i)) print*, m_i, mm, e_boundary_flags(reversed_e_bound_indexing_array(mm))
+                    !if (viable_mains(m_i)) print*, m_i, mm, e_boundary_flags(reversed_e_bound_indexing_array(mm))
+                    print*, m_i, mm, e_boundary_flags(reversed_e_bound_indexing_array(mm))
                 enddo
                 
                 ! and the same for internal faces but we dont need to worry about features
