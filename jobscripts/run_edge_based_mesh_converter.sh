6d413ef4f590e71870e57143b2d9921d2d3fb466
josh <josh8thomas@gmail.com>
2026-08-10 12:43:12 +0100

    123

--- jobscripts/run_edge_based_mesh_converter.sh
+++ jobscripts/run_edge_based_mesh_converter.sh
@@ -49,7 +49,7 @@ python3 "./python_scripts/case_preprocessor.py" -f case_file
 
 echo ' running main converter program serial '
 
-./converter case_file
+# ./converter case_file
 
 echo ' finished. '
 
