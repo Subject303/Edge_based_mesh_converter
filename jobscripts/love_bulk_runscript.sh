8a40ab77b26a4a668eb31e6e7daabdd8bc44472d
josh <josh8thomas@gmail.com>
2026-08-10 13:47:25 +0100

    123

---
+++ jobscripts/love_bulk_runscript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#PBS -N Mesh_converter
+#PBS -e logfolder/log.$PBS_JOBID.txt
+#PBS -j eo
+#PBS -m bf
+#PBS -M j.thomas@lboro.ac.uk
+#PBS -l walltime=20:00:00
+#PBS -l nodes=1:ppn=40
+#PBS -A Szmelter2025b
+
+cd      $PBS_O_WORKDIR
+
+case_file="2_rad_sphere_poly.case"
+
+sh "./run_edge_based_mesh_converter.sh" $case_file
+
+cd      $PBS_O_WORKDIR
+
+case_file="2_rad_sphere_poly.case"
+
+sh "./run_edge_based_mesh_converter.sh" $case_file
+
+cd      $PBS_O_WORKDIR
+
+case_file="2_rad_sphere_poly.case"
+
+sh "./run_edge_based_mesh_converter.sh" $case_file
