9d01d134925b8b7777bb19167563e0d08988bf79
josh <josh8thomas@gmail.com>
2026-08-11 18:40:07 +0100

    123

--- src/src_shared/volume_processing.f90
+++ src/src_shared/volume_processing.f90
@@ -118,8 +118,6 @@ module volume_processing
             e = e_bound_indexing_array(be)
             ! this is a loop of all boundary edges.
             
-            sb(be,:) = 0.0
-            cycle
             
             i1 = e_p_obj_relation_array(e_p_index_array(e)-1)
             i2 = e_p_obj_relation_array(e_p_index_array(e))
@@ -181,8 +179,6 @@ module volume_processing
             p = p_bound_indexing_array(bp)
             ! this is a loop of all boundary edges.
             
-            sbb(bp,:) = 0.0
-            cycle
             
             if (feature_points(bp)) then
                 ! corners
