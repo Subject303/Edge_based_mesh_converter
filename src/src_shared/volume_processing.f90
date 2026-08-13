62ae6c642078ad935ca425bac65ebef0dcd6e7b2
josh <josh8thomas@gmail.com>
2026-08-13 18:28:14 +0100

    132

--- src/src_shared/volume_processing.f90
+++ src/src_shared/volume_processing.f90
@@ -519,7 +519,7 @@ module volume_processing
             call centroid_swapper(mm, tt, fwd_i, bck_i, centroid_obj_array, state)
             
             if (obj_type.eq.featre_point) then
-                print*, mm, tt(1), tt(2), e_boundary_flags(reversed_e_bound_indexing_array(mm))
+                print*, flag, mm, tt(1), tt(2), e_boundary_flags(reversed_e_bound_indexing_array(mm))
                 print*, centroid_obj_array
                 print*, ' '
                 print*, ' '
