4d9afbfbb86e5f1a64a476e4e17eaa1188484996
josh <josh8thomas@gmail.com>
2026-09-02 14:33:22 +0100

    123

---
+++ jobscripts/local_run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# case_file="half_domain_slender.vtk"
+# case_file="full_domain.vtk"
+case_file="slender-no-wake-cour.vtk"
+# case_file="slender-no-wake-acc.vtk"
+# case_file="star1.case"
+
+sh "./run_edge_based_mesh_converter.sh" $case_file
