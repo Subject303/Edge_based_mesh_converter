8400c0fd9ecabc52e3f53e9d870cbaa6ef0cf5cf
josh <josh8thomas@gmail.com>
2026-09-17 17:13:41 +0100

    123

--- jobscripts/local_run.sh
+++ jobscripts/local_run.sh
@@ -2,7 +2,7 @@
 
 # case_file="half_domain_slender.vtk"
 # case_file="full_domain.vtk"
-case_file="slender-no-wake-cour.vtk"
+case_file="slender-no-wake-fine.vtk"
 # case_file="slender-no-wake-acc.vtk"
 # case_file="star1.case"
 
